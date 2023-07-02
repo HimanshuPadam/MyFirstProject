93d0853da1fb90cb67cc41fa8f0a80520d58c2e3
Himanshu Padam <himanshupadam808@gmail.com>
2023-07-02 17:22:59 +0530

    some modifications

--- app/src/main/java/com/himanshu/myfirstproject/CheckboxRadioActivity.kt
+++ app/src/main/java/com/himanshu/myfirstproject/CheckboxRadioActivity.kt
@@ -1,16 +1,20 @@
 package com.himanshu.myfirstproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.Toast
 
 class CheckboxRadioActivity : AppCompatActivity() {
     var cbIAgree:CheckBox?=null
+    var btnSubmitMove: Button?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkbox_radio)
         cbIAgree=findViewById(R.id.cbIAgree)
+        btnSubmitMove=findViewById(R.id.btnSubmit)
         cbIAgree?.setOnCheckedChangeListener { buttonView, isChecked ->
             if(isChecked){
                 Toast.makeText(this, "Checked",Toast.LENGTH_LONG).show()
@@ -19,5 +23,8 @@ class CheckboxRadioActivity : AppCompatActivity() {
                 Toast.makeText(this, "Error! Not Checked",Toast.LENGTH_LONG).show()
             }
         }
+       btnSubmitMove?.setOnClickListener {
+           var intent= Intent(this, )
+       }
     }
 }
