09003ddbc3fe4f344c52c944bc041d5b4620adbb
Himanshu Padam <himanshupadam808@gmail.com>
2023-07-07 13:23:55 +0530

    Use of Snackbar

--- app/src/main/java/com/himanshu/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/himanshu/myfirstproject/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     var btnValidate: Button?= null
     var etMarks: EditText?= null
     var etPercentage: EditText?= null
+    var btnClear: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         btnValidate=findViewById(R.id.btnValidate)
         etMarks=findViewById(R.id.etMarks)
         etPercentage=findViewById(R.id.etPercentage)
+        btnClear=findViewById(R.id.btnClear)
 
         btnValidate?.setOnClickListener {
             if (etName?.text.isNullOrEmpty()) {
@@ -47,6 +50,18 @@ class MainActivity : AppCompatActivity() {
                 //finish()
             }
         }
-
-    }
+        btnClear?.setOnClickListener{
+            btnClear?.let{
+                Snackbar.make(it,"Information cleared successfully",Snackbar.LENGTH_INDEFINITE)
+                    .setAction("OK") {
+                    Toast.makeText(this, "Fill it again now!!!!!!!!!", Toast.LENGTH_SHORT).show()
+                        etName?.setText("")
+                        etPhoneNo?.setText("")
+                        etMarks?.setText("")
+                        etPercentage?.setText("")
+                        etCollegeInfo?.setText("")
+                }.show()
+            }
+        }
+        }
 }
